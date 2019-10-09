b67f115274794bec192bd65b182e85a49a3d42d9
jackmin <1416825008@qq.com>
2019-10-09 08:49:39 +0800

    提供服务端接受请求后返回json列表

---
+++ Webapi学习记录/服务端返回json列表/Video_Info.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFC_ID
+{
+    public class Video_Info_Data
+    {
+        private string myid = "";
+        private string myusername = "";
+        private string mypassword = "";
+        private string myrtspUrl = "";
+        private string myisOpen = "";
+
+        public string id { get { return myid; } set { myid = value; } }
+        public string username { get { return myusername; } set { myusername = value; } }
+        public string password { get { return mypassword; } set { mypassword = value; } }
+        public string rtspUrl { get { return myrtspUrl; } set { myrtspUrl = value; } }
+        public string isOpen { get { return myisOpen; } set { myisOpen = value; } }
+    }
+    public class Video_Info
+    {
+        private string mycode = "";
+        private string mymsg = "";
+        private Video_Info_Data mydata;
+        public Video_Info() { data = new Video_Info_Data(); }
+        // 定义嵌套json里面的data
+        public string code { get { return mycode; } set { mycode = value; } }
+        public string msg { get { return mymsg; } set { mymsg = value; } }
+        public Video_Info_Data data { get { return mydata; } set { mydata = value; } }
+        
+    }
+}
---
+++ Webapi学习记录/服务端返回json列表/videoController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using System.Web.Script.Serialization;
+
+namespace NFC_ID.Controllers
+{
+    /// <summary>
+    /// 添加视频配置读取接口
+    /// </summary>
+    public class videoController : ApiController
+    {
+        [HttpPost]
+        public HttpResponseMessage findVideoConf(string ids)
+        {
+            string json = string.Empty;
+            // 根据ids，产生多个json对象
+            List<Video_Info> video_details = new List<Video_Info>();
+            
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            string[] ids_split = ids.Split(',');
+            foreach (string ids_index in ids_split)
+            {
+                Video_Info video_Info = new Video_Info();
+                switch (ids_index)
+                {
+                    case "1":{
+                            video_Info.code = "113";
+                            video_Info.msg = "查询成功!";
+                            video_Info.data.id = "1";
+                            video_Info.data.username = "admin";
+                            video_Info.data.password = "adm5201020";
+                            video_Info.data.rtspUrl = "rtsp://admin:adm12345@192.168.5.4:554/Streaming/Channels/101";
+                            video_Info.data.isOpen = "1";break;
+                        }
+                    case "2": {
+                            video_Info.code = "113";
+                            video_Info.msg = "查询成功!";
+                            video_Info.data.id = "2";
+                            video_Info.data.username = "admin";
+                            video_Info.data.password = "adm5201020";
+                            video_Info.data.rtspUrl = "rtsp://admin:adm12345@192.168.5.4:554/Streaming/Channels/102";
+                            video_Info.data.isOpen = "1";break;
+                        }
+                    default:{
+                            video_Info.code = "114";
+                            video_Info.msg = "无结果";
+                            video_Info.data = null;
+                            break;
+                        }
+                } // switch
+                video_details.Add(video_Info);
+            }
+            // 序列化一个list可以用于返回 多个json
+            json = serializer.Serialize(video_details);
+            return new HttpResponseMessage { Content = new StringContent(json, Encoding.GetEncoding("UTF-8"), "application/json") };
+        }
+    }
+}
